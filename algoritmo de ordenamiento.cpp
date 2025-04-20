246f575607d54817ddd61530f0f3f6c7e88c7745
Ariel Orellana <lpenao@miumg.edu.gt>
2025-04-20 14:21:57 -0600

    Cambio en el bucle
    Se modificó la lógica del bucle principal añadiendolo como un ciclo while

--- algoritmo de ordenamiento.cpp
+++ algoritmo de ordenamiento.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
-#include <vector>
+#include <vector> // Librería para usar vectores
 
 using namespace std;
-int intercambio = 1;
+bool hubo_intercambio = true; /// se usará para el bucle while, covertida en respuesta de si o no 
 
 int main(){
-    //vector<int> lista= {};
-    //vector<int> lista = {3, 5, 17, 2, 9, 8};
 
-    vector<int> lista = {3, 5, 17, 2, 9, 8};
+    vector<int> lista = {3, 5, 17, 2, 9, 8, 23, 7, 11}; // Vector de enteros a ordenar y poder usar las funciones de la librería vector
 
-    for( int i = 1; intercambio!=0; i++){
-        for (int j = 0; j < lista.size(); i++){
-            int a = lista[i];
-            int b = lista[i+1];
-            int intercambio = 0;
+    while (hubo_intercambio){
+        hubo_intercambio = false;
+            for (int i = 0; i < lista.size()-1; i++){
+                int a = lista[i];
+                int b = lista[i+1];
 
-            if(a>b){
-                lista[i] = b;
-                lista[i+1] = a;
-                intercambio = 1;
+                if(a>b){
+                    lista[i] = b;
+                    lista[i+1] = a;
+                    hubo_intercambio = true;
+                }            
             }
-
         }
+    cout << "Lista ordenada: " << endl;
+    for (int i = 0; i < lista.size(); i++){
+        cout << lista[i] << ", "; 
+    }    
     }
-}
